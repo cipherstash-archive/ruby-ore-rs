f2c8d0b4e438dabd004488d77e3cf052053ebb5a
Matt Palmer <matt@cipherstash.com>
2022-05-01 12:52:21 +1000

    Work around limitations in gem-compiler for macOS systems
    Turns out gem-compiler is a little _too_ specific in it's choice of architectures,
    so we're going to do it ourselves instead, which also reduces the amount of
    faffing around required elsewhere.

--- lib/ore-rs.rb
+++ lib/ore-rs.rb
@@ -1,7 +1,13 @@
-require "rutie"
+require "fiddle"
 
-module ORE
-  Rutie.new(:ruby_ore_rs).init 'Init_ruby_ore_rs', __dir__
+begin
+  Fiddle::Function.new(Fiddle.dlopen("#{__dir__}/#{RbConfig::CONFIG["ruby_version"]}/libore_rs.#{RbConfig::CONFIG["SOEXT"]}")["Init_libore_rs"], [], Fiddle::TYPE_VOIDP).call
+rescue Fiddle::DLError
+  begin
+    Fiddle::Function.new(Fiddle.dlopen("#{__dir__}/libore_rs.#{RbConfig::CONFIG["SOEXT"]}")["Init_libore_rs"], [], Fiddle::TYPE_VOIDP).call
+  rescue Fiddle::DLError
+    raise LoadError, "Failed to initialize libore_rs.#{RbConfig::CONFIG["SOEXT"]}; either it hasn't been built, or was built incorrectly for your system"
+  end
 end
 
 require_relative "./ore/aes128"
